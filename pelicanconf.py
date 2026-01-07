29385af48334d877efcbda64466915d82940b057
Ryan Lanigan <31903355+ryanclanigan@users.noreply.github.com>
2026-01-07 09:32:24 -0700

    Revert "Update links"
    This reverts commit b4a261f747d833d4e050f13d4bf1d617d4ba1f9c.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'Ryan Lanigan'
 SITENAME = "Ryan's Thoughts"
-SITEURL = "lanigan.xyz"
+SITEURL = ""
 
 PATH = "content"
 
@@ -18,11 +18,15 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (
     ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ("No", "#"),
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
 )
 
 DEFAULT_PAGINATION = False
